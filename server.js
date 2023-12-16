92e1d8031a460c0fc264cffbb531ff6ccc4a3073
kudtarkarsahil <sahilkudtarkar315@gmail.com>
2023-12-16 18:15:25 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -12,9 +12,12 @@ const teacherRoutes = require("./routes/teacher.routes");
 const studentRoutes = require("./routes/student.routes");
 
 // middlewares
+app.use("*", cors({
+  origin: true,
+  credentials: true,
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 app.use(cookieParser());
 
 // port
